46d6489996a5d747907c8d3a38c2bf17b21ee811
Alamin11 <alamincsecu@gmail.com>
2024-10-08 14:50:21 +0600

    added card component

--- react-practice-app-1/src/App.jsx
+++ react-practice-app-1/src/App.jsx
@@ -1,8 +1,24 @@
 import Header from './Header.jsx'
+import Footer from './Footer.jsx'
+import Food from './Food.jsx'
+import Card from './Card.jsx'
+
 import React from 'react';
 function App(){
   return (
+    <>
     <Header />
+    <Food />
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+    <Footer/>
+    </>
   );
 }
 export default App;
---
+++ react-practice-app-1/src/Card.jsx
@@ -0,0 +1,13 @@
+import cardDP from './assets/butterfly150.jpg'
+function Card(){
+
+    return(
+        <div className="card">
+        <img className='card-image' src={cardDP} alt="DP of card" />
+        <h2 className='card-title'>Card Title</h2>
+        <p className='card-text'>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Ducimus cumque magnam illo recusandae eveniet velit excepturi iste quae ipsam tempore.</p>
+        </div>
+    );
+}
+export default Card
