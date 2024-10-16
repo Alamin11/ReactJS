3216bca552956ca5b88355e6233f30f56c9111d7
Alamin11 <alamincsecu@gmail.com>
2024-10-16 17:39:15 +0600

    added list with props

--- react-practice-app-1/src/App.jsx
+++ react-practice-app-1/src/App.jsx
@@ -7,6 +7,21 @@ import UserGreeting from './UserGreetings.jsx'
 import List from './List.jsx'
 import React from 'react';
 function App(){
+
+  const fruits = [
+    {id:1, name: "Apple", calories: 95}, 
+    {id:2, name: "Orange", calories: 45}, 
+    {id:3, name: "Banana", calories:105}, 
+    {id:4, name: "Coconut", calories: 159}, 
+    {id:5, name: "Pineapple", calories: 37}];
+
+  const vegetables = [
+      {id:6, name: "Potato", calories: 110}, 
+      {id:7, name: "Cabbage", calories: 56}, 
+      {id:8, name: "Tomato", calories:35}, 
+      {id:9, name: "Brinjal", calories: 57}, 
+      {id:10, name: "Peas", calories: 80}];
+
   return (
     <>
     <Header />
@@ -15,8 +30,8 @@ function App(){
       userName="Al amin"
     />
     {/* <Food /> */}
-    <List/>
-    
+    {fruits.length > 0 && <List items={fruits} category= "Fruits"/>}
+    {vegetables.length > 0 && <List items={vegetables} category= "Vegetables"/>}
     <Card 
       title='Butterfly' 
       src={cardDP} 
--- react-practice-app-1/src/List.jsx
+++ react-practice-app-1/src/List.jsx
@@ -1,20 +1,25 @@
-function List(){
-const fruits = [
-    {name: "Apple", calories: 95}, 
-    {name: "Orange", calories: 45}, 
-    {name: "Banana", calories:105}, 
-    {name: "Coconut", calories: 159}, 
-    {name: "Pineapple", calories: 37}];
+function List(props){
+
 // fruits.sort();
 // fruits.sort((a,b)=> a.name.localeCompare(b.name)); //Alphabetic order
 // fruits.sort((a,b)=> b.name.localeCompare(a.name)); //reverse Alphabetic order
 // fruits.sort((a,b)=>a.calories - b.calories);//assending orders or numeric orders
 // fruits.sort((a,b)=> b.calories - a.calories);//desending orders or reverse numeric orders
 
-// const listItems = fruits.map(fruit=><li key={fruit.name}>{fruit.name}: &nbsp;<b>{fruit.calories}</b></li>);
+
 // Filtering list items
-const lowCalFruits = fruits.filter(localFruit => localFruit.calories < 100);
-const listItems = lowCalFruits.map(localFruit => <li key={localFruit.name}>{localFruit.name}: &nbsp; {localFruit.calories}</li>)
-return (<ol>{listItems}</ol>);
+// const lowCalFruits = fruits.filter(localFruit => localFruit.calories < 100);
+// const listItems = lowCalFruits.map(localFruit => <li key={localFruit.name}>{localFruit.name}: &nbsp; {localFruit.calories}</li>)
+// const highCalFruits = fruits.filter(highcalFruit => highcalFruit.calories > 100);
+// const listItems = highCalFruits.map(highcalFruit => <li key={highcalFruit.name}>{highcalFruit.name}: &nbsp; {highcalFruit.calories}</li>)
+const itemList = props.items;
+const category = props.category;
+const listItems = itemList.map(item=><li key={item.name}>{item.name}: &nbsp;<b>{item.calories}</b></li>);
+return (
+    <>
+        <h3 className="list-category">{category}</h3>
+        <ol className="list-item">{listItems}</ol>
+    </>
+);
 }
 export default List
