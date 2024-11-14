64909adb30a657de80418f085bedc3065aa8773c
Alamin11 <alamincsecu@gmail.com>
2024-11-14 17:58:47 +0600

    added config

---
+++ electra-shop/src/sections/Hero.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Headset from "../assets/headset.jpg";
+import earbuds from "../assets/earbuds.jpg";
+import dslr from "../assets/dslr.jpg";
+import AOS from "aos";
+import "aos/dist/aos.css";
+const Hero = () => {
+  return <div>Hero</div>;
+};
+export default Hero;
